14eac06343855848cc04ec473658aceb5434d43c
valendea <valendea.amalia@gmail.com>
2018-04-25 22:27:02 +0800

    rm password_digest from user column

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,16 @@ Rails.application.routes.draw do
   # root 'sessions#new'
   
   # Define the route to make it more friendly
-  get 'signup', to: 'users#new', as: 'signup'
-  get 'login', to: 'sessions#new', as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
-  get '/vote', to: 'votes#create', as: 'vote'
+  # get 'signup', to: 'users#new', as: 'signup'
+  # get 'login', to: 'sessions#new', as: 'login'
+  # get 'logout', to: 'sessions#destroy', as: 'logout'
+  # get '/vote', to: 'votes#create', as: 'vote'
 
 
 	get "/auth/:provider/callback" => "sessions#create_from_omniauth"
 
-  resources :users
-  resources :sessions, only: [:create]
+  # resources :users
+  # resources :sessions, only: [:create]
   resources :recipes
   resources :vote, only: [:create]
   root 'welcome#index'
